4bd050ba1eafd10c169be7c8e2f3169dffcdb7a7
mjsauro <mjsauro@gmail.com>
2017-11-18 15:42:12 -0600

    parsed the date and days of the week

--- javascript.js
+++ javascript.js
@@ -1,11 +1,14 @@
 $(document).ready(function () {
 
+    //location fired by default
     getLocation();
 
+    //getLocation function
     function getLocation() {
         navigator.geolocation.getCurrentPosition(showPosition);
     }
 
+    //position is translated lat and long
     function showPosition(position) {
         var lat = position.coords.latitude;
         var long = position.coords.longitude;
@@ -13,10 +16,9 @@ $(document).ready(function () {
             " Longitude: " + long);
 
         $.getJSON("https://query.yahooapis.com/v1/public/yql?q=SELECT * FROM geo.places WHERE text=\"(" + lat + "," + long + ")\"&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys", clear(), locationReceived);
-
-
     }
 
+    //latittude and longitude converted to city
     function locationReceived(response) {
 
         console.log(response.query.results.place.locality1.content);
@@ -27,29 +29,27 @@ $(document).ready(function () {
     }
 
 
-
+    //on submit, the city and state are detected.  if values are blank, location is detected.
     $("#submit").on('click', function () {
         console.log("click fired!");
         if (($('#city-input').val() == '') && ($('#state-input').val() == '')) {
             console.log("submit null");
             getLocation();
-
         } else {
-
             var value = $('#city-input').val();
             var value2 = $('#state-input').val();
             console.log("submit fired");
             $.getJSON('https://query.yahooapis.com/v1/public/yql?q=select * from weather.forecast where woeid in (select woeid from geo.places(1) where text="' + value + ', ' + value2 + '")&format=json', clear(), weatherTry);
         }
-
-
     })
 
+    //location is detected
     $("#detect").on('click', function () {
         console.log("Detect fired!");
         getLocation();
     })
 
+    //title is cleared
     function clear() {
         $(".title").empty();
         $(".currenttemp").empty();
@@ -58,6 +58,7 @@ $(document).ready(function () {
         $("article > div > div > div").empty();
     }
 
+    //weather response
     function weatherTry(response) {
         //title
         $(".title").append(response.query.results.channel.description);
@@ -68,10 +69,12 @@ $(document).ready(function () {
         var high = ("High: ");
         var low = ("Low: ");
 
-        var forecast = response.query.results.channel.item.forecast[0]
+        var forecast = response.query.results.channel.item.forecast[0];
+
+
         //curent conditions
         var current = response.query.results.channel.item.condition;
-        $(".currenttemp").prepend(current.temp);
+        $(".currenttemp").prepend((current.temp) + "F&deg;");
         $(".currenttext").append(current.text);
         $(".current-icon").append(code);
 
@@ -83,15 +86,23 @@ $(document).ready(function () {
         for (var i = 0; i <= 9; i++) {
             console.log("try fired");
             var forecast = response.query.results.channel.item.forecast[i];
+            var dayOfWeek = forecast.day;
+            //rearrange the date
+            var date = forecast.date;
+            var day = date.slice(0, 2);
+            var month = date.slice(2, 6);
+            var year = date.slice(7, 11);
+            var newDate = (month + " " + day + ", " + year)
+
             var panel = document.createElement("div");
             panel.className = 'dayBox col-xs-4 col-sm-2 text-center';
             var panel
             $(".weatherTemplate").append(panel);
             var createDiv = document.createElement("div");
-            $(createDiv).addClass("weatherDate");
-            $(createDiv).append(forecast.date + "<br/>").appendTo(panel);
             $(createDiv).addClass("weatherDay")
-            $(createDiv).append(forecast.day + "<br/>").appendTo(panel);
+            $(createDiv).append(getDay(dayOfWeek) + "<br /> ").appendTo(panel);
+            $(createDiv).addClass("weatherDate");
+            $(createDiv).append(newDate + "<br/>").appendTo(panel);
             $(createDiv).addClass("weatherHigh ");
             $(createDiv).append((high) + (forecast.high + (' F&deg') + "<br/>")).appendTo(panel);
             $(createDiv).addClass("weatherLow ");
@@ -99,6 +110,33 @@ $(document).ready(function () {
             $(createDiv).addClass("weatherText ");
             $(createDiv).append(forecast.text + "<br/>").appendTo(panel);
             $(createDiv).append(code).appendTo(panel);
+
+            //get the full name of the day of the week
+            function getDay(item) {
+                switch (item) {
+                    case "Sun":
+                        return "Sunday, ";
+                        break;
+                    case "Mon":
+                        return "Monday, ";
+                        break;
+                    case "Tue":
+                        return "Tuesday, ";
+                        break;
+                    case "Wed":
+                        return "Wednesday, ";
+                        break;
+                    case "Thu":
+                        return "Thursday, ";
+                        break;
+                    case "Fri":
+                        return "Friday, ";
+                        break;
+                    case "Sat":
+                        return "Saturday, ";
+                        break;
+                }
+            }
         }
 
         function code() {
