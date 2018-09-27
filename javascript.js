cac2a0df68b693e5605cc113ca10f041cde4c242
mjsauro <mjsauro@gmail.com>
2018-09-26 21:38:42 -0500

    did lots of stuff

--- javascript.js
+++ javascript.js
@@ -1,8 +1,5 @@
 $(document).ready(function () {
     
-    var now = moment().format('MM DD YY');
-
-    $("#date").append(now);
     getLocation();
 
 });
@@ -14,33 +11,64 @@ function getLocation() {
 function showPosition(position) {
     var lat = position.coords.latitude;
     var long = position.coords.longitude;
-    console.log(position);
-    console.log(lat);
-    console.log(long);
-    getWeather(lat, long);
+    getCurrentWeather(lat, long);
+    //getWeather(lat, long);
 
 }
 
-function getWeather(lat, long) 
-{
+function getCurrentWeather(lat, long) {
     $.ajax({
-        url: "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + long + "&APPID=f359c189ba425c0490e35966335db4c7",
+        url: "https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + long + "&units=imperial" + "&APPID=f359c189ba425c0490e35966335db4c7",
         type: "GET",
         data: {
             format: 'json'
         },
         success: function(response) {
             console.log(response);
-            console.log(response.city.name);
-            console.log();
+            
+            var cityName = response.name;
+            $("#city-name").append(cityName);
 
-            for (var i = 0; i < 5; i++);
-            var date1 = (moment.unix(response.list[0].dt).format("LLL"));
-            $("#date1").append(date1);
+            var currentDateAndTime = moment.unix(response.dt).format('MMMM Do YYYY, h:mm a');
+            $("#current-date-and-time").append(currentDateAndTime);
 
+            var currentCondition = response.weather[0].description;
+            $("#current-condition").append(currentCondition.toTitleCase());
+
+            var iconCode = response.weather[0].icon;
+            var dayOrNight = iconCode.substring(iconCode.length - 1);
+            $("#current-icon").append("<i class='owf owf-4x owf-" + response.weather[0].id + "-" + dayOrNight + "'></i>");
+
+            var currentTemp = response.main.temp;
+            $("#current-temp").append(parseInt(currentTemp) + "&#8457;");
+            
         },
         error: function(error) {
             console.log(error);
         }
     });      
 }
+
+// function getWeather(lat, long) 
+// {
+//     $.ajax({
+//         url: "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + long + "&APPID=f359c189ba425c0490e35966335db4c7",
+//         type: "GET",
+//         data: {
+//             format: 'json'
+//         },
+//         success: function(response) {
+//             console.log(response);
+//             console.log(response.city.name);
+//             console.log();
+
+//             for (var i = 0; i < 5; i++);
+//             var date1 = (moment.unix(response.list[0].dt).format("LLL"));
+//             $("#date1").append(date1);
+
+//         },
+//         error: function(error) {
+//             console.log(error);
+//         }
+//     });      
+// }
